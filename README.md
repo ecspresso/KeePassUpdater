228952aa34b5554bc851808f856ce9759135a90a
ecspresso <emile@ecspreso.com>
2022-09-29 15:05:27 +0200

    Removed all unmaintained files.
    I am no longer developing in python and I am not running this as a exe.
    Thus all python, exe and image files have been removed from the current
    version, only the ps1 files are maintained. All other files can be
    found in commit #ee0dcb4e5305ed8fc3633adef7c9ed0f74273292 or earlier.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # KeePassUpdater
-Keep KeePass and it's plugins up-do-date with powershell or python.
+Keep KeePass and it's plugins up-do-date with powershell.
+**REQUIRES NANAZIP TO UPDATE KEEPASS**
+The version file is packaged as a .gz and requires nanazip (or something else but I use nanazip) to extract.
--- ps1toexe.ps1
+++
@@ -1,8 +0,0 @@
-$ps1 = Get-ChildItem -Path '.\PowerShell' -Filter "*.ps1"
-$exe = Get-ChildItem -Path '.\exe'
-
-foreach($file in $ps1) {
-    if($exe.BaseName -notcontains $file.BaseName) {
-        Invoke-ps2exe -inputFile $file.FullName -noConsole -noOutput -noVisualStyles -requireAdmin -outputFile "$($file.DirectoryName | Split-Path -Parent)\exe\$($file.BaseName).exe" -iconFile '.\icon.ico' -verbose
-    }
-}
